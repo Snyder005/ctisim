c6e627f7fdc6f9b9b64f7238c0476abdc2752f4d
snyder18 <snyder18@stanford.edu>
2020-03-30 14:20:29 -0700

    Removed old scripts, added new script for correction testing.  Removed old notebooks.

--- python/ctisim/__init__.py
+++ python/ctisim/__init__.py
@@ -1,4 +1,4 @@
-from ctisim.core import LinearTrap, LogisticTrap
+from ctisim.core import LinearTrap, LogisticTrap, SplineTrap
 from ctisim.core import FloatingOutputAmplifier, BaseOutputAmplifier
 from ctisim.image import ImageSimulator, SegmentSimulator
 from ctisim.utils import ITL_AMP_GEOM
